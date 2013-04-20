182b50357ae02dbdc704faf7946c7606c4d4f400
Nils Brünggel <n.bruenggel@gmail.com>
2013-04-20 22:50:27 +0200

    iterative version

--- Rust/Fib.rs
+++ Rust/Fib.rs
@@ -1,13 +1,35 @@
-fn fib(n: int) -> int {
-	
+fn rFib(n: int) -> int {
 	match n {
-		0 => return 0,
-		1 => return 1,
-		_ => return fib(n - 1) + fib(n - 2)
+		0 => 0,
+		1 => 1,
+		_ => rFib(n - 1) + rFib(n - 2)
+	}
+}
+
+fn fib(n: int) -> int {
+
+	fn doNormalCase(n: int) -> int {
+		let mut prev = 1, prevPrev = 0, i = 2;
+
+		while i != n+1 {
+			let tmp = prev + prevPrev;
+			prevPrev = prev;
+			prev = tmp;
+			i += 1;
+		}
+
+		prev
+	}
+
+	match n {
+		0 => 0,
+		1 => 1,
+		_ => doNormalCase(n)
 	}
 }
 
 fn main() {
+	//let n = rFib(7);
 	let n = fib(7);
 	io::println(fmt!("%d", n));
 }
